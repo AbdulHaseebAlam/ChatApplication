e21e714740ca640437875523fcacf2979152a358
AbdulHaseebAlam <ah03092412589@gmail.com>
2024-07-06 16:16:21 +0500

    Update packages, app controller, service, main file

--- chat-application/src/app.controller.ts
+++ chat-application/src/app.controller.ts
@@ -1,9 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  InternalServerErrorException,
+  Param,
   Post,
   UseFilters,
 } from '@nestjs/common';
@@ -23,7 +26,94 @@ export class AppController {
 
   @HttpCode(HttpStatus.OK)
   @Post('prompt')
-  getPromptResponse(@Body() body: PromptBody) {
-    return this.appService.getPromptResponse(body.prompt);
+  async getPromptResponse(@Body() body: PromptBody) {
+    try {
+      const { response, title } = await this.appService.getPromptResponse(
+        body.prompt,
+      );
+
+      // Create a new chat session
+      const chatSession = await this.appService.createChatSession(title);
+
+      // Save the message to the chat session
+      await this.appService.createMessage(
+        chatSession.id,
+        body.prompt,
+        response,
+      );
+      return { response, title, chatSessionId: chatSession.id };
+    } catch (error) {
+      console.error('Error in getPromptResponse:', error);
+      throw new InternalServerErrorException(
+        'Failed to process prompt response',
+      );
+    }
+  }
+
+  @Post('chat-sessions/:id/messages')
+  async addMessageToChatSession(
+    @Param('id') chatSessionId: string,
+    @Body() body: PromptBody,
+  ) {
+    try {
+      const chatSessionIdNum = parseInt(chatSessionId, 10);
+      const { response, title } = await this.appService.getPromptResponse(
+        body.prompt,
+      );
+
+      // Save the message to the chat session
+      const message = await this.appService.createMessage(
+        chatSessionIdNum,
+        body.prompt,
+        response,
+      );
+
+      return { response, title, message };
+    } catch (error) {
+      console.error('Error in addMessageToChatSession:', error);
+      throw new InternalServerErrorException(
+        'Failed to add message to chat session',
+      );
+    }
+  }
+
+  @Get('chat-sessions')
+  async getChatSessions() {
+    try {
+      return this.appService.getChatSessions();
+    } catch (error) {
+      console.error('Error in getChatSessions:', error);
+      throw new InternalServerErrorException(
+        'Failed to retrieve chat sessions',
+      );
+    }
+  }
+
+  @Get('chat-sessions/:id')
+  async getMessagesByChatSessionId(@Param('id') chatSessionId: string) {
+    try {
+      const chatSessionIdNum = parseInt(chatSessionId, 10);
+      const chatSession =
+        await this.appService.getMessagesByChatSessionId(chatSessionIdNum);
+      return chatSession;
+    } catch (error) {
+      console.error('Error in getMessagesByChatSessionId:', error);
+      throw new InternalServerErrorException(
+        'Failed to retrieve messages for chat session',
+      );
+    }
+  }
+
+  @Delete('chat-sessions/:id')
+  async deleteChatSession(@Param('id') chatSessionId: string) {
+    try {
+      const chatSessionIdNum = parseInt(chatSessionId, 10);
+
+      await this.appService.deleteChatSession(chatSessionIdNum);
+      return { message: 'Chat session deleted successfully' };
+    } catch (error) {
+      console.error('Error in deleteChatSession:', error);
+      throw new InternalServerErrorException('Failed to delete chat session');
+    }
   }
 }
--- chat-application/src/app.service.ts
+++ chat-application/src/app.service.ts
@@ -1,35 +1,139 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ChatSession, Message, PrismaClient } from '@prisma/client';
+import { marked } from 'marked';
 
 @Injectable()
 export class AppService {
+  private prisma: PrismaClient;
   private genAI: any;
   private geminiPro: any;
+  private hrInstruction: string;
+
   constructor(private readonly config: ConfigService) {
+    this.prisma = new PrismaClient();
     this.genAI = new GoogleGenerativeAI(this.config.get('API_KEY'));
 
     this.geminiPro = this.genAI.getGenerativeModel({
       model: 'gemini-1.5-flash',
     });
+
+    this.hrInstruction = this.config.get<string>('HR_INSTRUCTION');
   }
 
   getHello(): string {
     return 'Hello World!';
   }
 
-  async getPromptResponse(prompt: string): Promise<string> {
+  async createChatSession(title: string): Promise<ChatSession> {
     try {
-      const result = await this.geminiPro.generateContent(prompt);
+      return await this.prisma.chatSession.create({
+        data: {
+          title,
+        },
+      });
+    } catch (error) {
+      console.error('Error creating chat session:', error);
+      throw new InternalServerErrorException('Failed to create chat session');
+    }
+  }
+
+  async createMessage(
+    chatSessionId: number,
+    userPrompt: string,
+    apiResponse: string,
+  ): Promise<Message> {
+    try {
+      return await this.prisma.message.create({
+        data: {
+          chatSessionId,
+          userPrompt,
+          apiResponse,
+        },
+      });
+    } catch (error) {
+      console.error('Error creating message:', error);
+      throw new InternalServerErrorException('Failed to create message');
+    }
+  }
+
+  async getPromptResponse(
+    prompt: string,
+  ): Promise<{ response: string; title: string }> {
+    const fullprompt = `${this.hrInstruction} ${prompt}`;
+    try {
+      const result = await this.geminiPro.generateContent(fullprompt);
       const response = await result.response;
       const text = response.text();
-      // console.log(text);
-      return text;
+
+      const parsedText = await marked(text);
+
+      const title = this.generateTitle(prompt, text);
+
+      return { response: parsedText, title };
     } catch (error) {
       console.error('Error generating prompt response:', error);
       throw new InternalServerErrorException(
         'Failed to generate prompt response',
       );
+    }
+  }
+
+  private generateTitle(prompt: string, response: string): string {
+    return `${prompt.split(' ').slice(0, 20).join(' ')}...`;
+  }
+
+  async getChatSessions(): Promise<ChatSession[]> {
+    try {
+      return await this.prisma.chatSession.findMany({
+        include: { messages: true },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+    } catch (error) {
+      console.error('Error retrieving chat sessions:', error);
+      throw new InternalServerErrorException(
+        'Failed to retrieve chat sessions',
+      );
+    }
+  }
+
+  async getMessagesByChatSessionId(
+    chatSessionId: number,
+  ): Promise<ChatSession> {
+    try {
+      return await this.prisma.chatSession.findUnique({
+        where: { id: chatSessionId },
+        include: { messages: true },
+      });
+    } catch (error) {
+      console.error('Error retrieving messages for chat session:', error);
+      throw new InternalServerErrorException(
+        'Failed to retrieve messages for chat session',
+      );
+    }
+  }
+
+  async deleteChatSession(chatSessionId: number): Promise<void> {
+    try {
+      await this.prisma.$transaction(async (prisma) => {
+        await prisma.message.deleteMany({
+          where: {
+            chatSessionId,
+          },
+        });
+        await prisma.chatSession.delete({
+          where: {
+            id: chatSessionId,
+          },
+        });
+      });
+    } catch (error) {
+      console.error('Error deleting chat session:', error);
+      throw new InternalServerErrorException('Failed to delete chat session');
     }
   }
 }
